5250cc3f8640eceeb728489a6bd1cb57e43b6404
Atria <atriasft@gmail.com>
2020-12-16 04:07:41 +0900

    戻るボタンの実装 #64

--- front/Android/app/src/main/java/team01/yowayowa/sekibutton/SettingsActivity.kt
+++ front/Android/app/src/main/java/team01/yowayowa/sekibutton/SettingsActivity.kt
@@ -3,16 +3,23 @@ package team01.yowayowa.sekibutton
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.util.Log;
+import android.view.MenuItem
 
 class SettingsActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)//戻るボタンの有効化
 
         // 設定用の Fragment を表示
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.settingsContainer, PreferenceFragment())
             .commit()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
     }
 }
