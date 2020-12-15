3889e4acb99da44b616e2d11bb4a244b09ae96ed
Atria <atriasft@gmail.com>
2020-12-16 02:40:29 +0900

    camera2を利用したライトの点灯/消灯を実装

--- front/Android/app/src/main/java/team01/yowayowa/sekibutton/FirstFragment.kt
+++ front/Android/app/src/main/java/team01/yowayowa/sekibutton/FirstFragment.kt
@@ -1,18 +1,30 @@
 package team01.yowayowa.sekibutton
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CameraManager.TorchCallback
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageButton
-import androidx.navigation.fragment.findNavController
+import android.widget.Toast
+import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.fragment.app.Fragment
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+
+    private lateinit var  McameraManager : CameraManager
+    private var McameraID: String? = null
+    private var SW : Boolean = false
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -25,8 +37,51 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        McameraManager = activity?.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        //ライト点灯用
+        McameraManager.registerTorchCallback(object : TorchCallback() {
+            override fun onTorchModeChanged(
+                cameraId: String,
+                enabled: Boolean
+            ) {
+                super.onTorchModeChanged(cameraId, enabled)
+                McameraID = cameraId
+                SW = enabled
+            }
+        }, Handler())
+
         view.findViewById<ImageButton>(R.id.imageButton).setOnClickListener {
-            //TODO("ここにタップ時処理")
+            requestCameraPermission()
+        }
+    }
+
+    private fun requestCameraPermission() {
+        // カメラ権限あり
+        if (checkSelfPermission(requireContext(),Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            flushTorch()
+            return
+        }
+        // カメラ権限なし
+        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+            Toast.makeText(context,"権限なし",Toast.LENGTH_SHORT).show()
+            return
+        }
+        requestPermissions(arrayOf(Manifest.permission.CAMERA), 200)
+    }
+
+    private fun flushTorch(){
+        if (McameraID == null) {
+            return
+        }
+        try {
+            if (!SW) {
+                McameraManager.setTorchMode(McameraID!!, true)
+            } else {
+                McameraManager.setTorchMode(McameraID!!, false)
+            }
+        } catch (e: CameraAccessException) {
+            //エラー処理
+            e.printStackTrace()
         }
     }
 }
