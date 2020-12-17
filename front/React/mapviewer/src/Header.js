40c3fb09994a10427f35fd2ad94e1d8e53b0503b
Atria <49768768+Atria64@users.noreply.github.com>
2020-12-17 17:14:37 +0900

    css調整,タイトル設置

--- front/React/mapviewer/src/Header.js
+++ front/React/mapviewer/src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './index.css';
 
 class Header extends React.Component {
   render() {
     return (
       <div className='header'>
-        <h1>this is header.</h1>
+        <div className='title'>
+            ACHOOO!
+        </div>
       </div>
     );
   }
