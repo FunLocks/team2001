5c91b52a388435d844ad26c019061a4b2d6605b3
Atria <49768768+Atria64@users.noreply.github.com>
2020-12-17 20:46:32 +0900

    sidebarに命名変更

--- front/React/mapviewer/src/Header.js
+++ front/React/mapviewer/src/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <div className='header'>
+      <div className='sidebar'>
         <div className='title'>
             AHCHOO!
         </div>
