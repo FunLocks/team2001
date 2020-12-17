04661eb15eb65d739196a70e9daca60d48849def
Atria <49768768+Atria64@users.noreply.github.com>
2020-12-17 16:33:57 +0900

    headerとmainで分けた

---
+++ front/React/mapviewer/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className='header'>
+        <h1>this is header.</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ front/React/mapviewer/src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className='main'>
+        <h2>this is main.</h2>
+      </div>
+    );
+  }
+}
+
+export default Main;
