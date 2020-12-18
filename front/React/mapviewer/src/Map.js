144609c696181cf6bfed4f60ed1f0db3bc661e50
Atria <atriasft@gmail.com>
2020-12-18 15:04:00 +0900

    全画面flex化

--- front/React/mapviewer/src/Map.js
+++ front/React/mapviewer/src/Map.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
- 
+
+const containerStyle = {
+  position: 'relative',  
+  width: '100%',
+  height: '100%'
+}
 export class MapContainer extends React.Component {
   render() {
     return (
-      <Map google={this.props.google} zoom={14}>
- 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
-      </Map>
+      <div className="container">
+        <Map google={this.props.google} zoom={14} containerStyle={containerStyle}>
+          <Marker onClick={this.onMarkerClick}
+           name={'Current location'} />
+        </Map>
+      </div>
     );
   }
 }
