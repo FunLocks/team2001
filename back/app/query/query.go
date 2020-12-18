1f14e9398d31133da8aa05a11a4ea1ff60031ec7
t4t5u0 <ymmtryk0902@gmail.com>
2020-12-19 07:08:10 +0900

    [add] エンドポイント張替え

--- back/app/query/query.go
+++ back/app/query/query.go
@@ -2,6 +2,7 @@ package query
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 
@@ -55,13 +56,10 @@ func GetOneRecord(c *gin.Context) {
 	// DBから取得する処理
 	db := gormdb.GormConnect()
 	db2, _ := db.DB()
-	result := map[string]interface{}{}
+	result := gormdb.Location{}
 	db.Model(&gormdb.Location{}).Last(&result)
 
 	defer db2.Close()
-	// return func(c *gin.Context) {
-	// 	c.JSON(200, result)
-	// }
 	c.JSON(http.StatusOK, result)
 }
 
@@ -76,4 +74,50 @@ func GetAllRecord(c *gin.Context) {
 	// return func(c *gin.Context) {
 	c.JSON(http.StatusOK, result)
 	// }
+}
+
+// GetOneHour 1時間分
+func GetOneHour(c *gin.Context) {
+	db := gormdb.GormConnect().Model(&gormdb.Location{})
+	db2, _ := db.DB()
+	result := []gormdb.Location{}
+
+	hour := time.Now().Add(-time.Hour).Format(time.RFC3339)
+	db.Order("id").Find(&result).Where("created_at > ?", hour)
+	defer db2.Close()
+	c.JSON(http.StatusOK, result)
+}
+
+// GetOneDay 1日分
+func GetOneDay(c *gin.Context) {
+	db := gormdb.GormConnect().Model(&gormdb.Location{})
+	db2, _ := db.DB()
+	result := []gormdb.Location{}
+	// today := time.Now().AddDate(0, 0, -1)
+	// db.Order("id").Find(&result).Where("created_at > ?", today)
+	db.Order("id").Find(&result).Where("created_at BETWEEN 2000-01-01T00:00:00Z AND 2000-01-08T00:00:00Z")
+	defer db2.Close()
+	c.JSON(http.StatusOK, result)
+}
+
+// GetSevenDays 7日分
+func GetSevenDays(c *gin.Context) {
+	db := gormdb.GormConnect().Model(&gormdb.Location{})
+	db2, _ := db.DB()
+	result := []gormdb.Location{}
+	lastWeek := time.Now().AddDate(0, 0, -7)
+	db.Order("id").Find(&result).Where("created_at > ?", lastWeek)
+	defer db2.Close()
+	c.JSON(http.StatusOK, result)
+}
+
+// GetThirtyDays 30日分
+func GetThirtyDays(c *gin.Context) {
+	db := gormdb.GormConnect().Model(&gormdb.Location{})
+	db2, _ := db.DB()
+	result := []gormdb.Location{}
+	lastMonth := time.Now().AddDate(0, -1, 0)
+	db.Order("id").Find(&result).Where("created_at", lastMonth)
+	defer db2.Close()
+	c.JSON(http.StatusOK, result)
 }
