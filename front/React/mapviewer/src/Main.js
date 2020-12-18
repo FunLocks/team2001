7424fb91adc246b7d931f6bb0941a09218ec1cc3
Atria <49768768+Atria64@users.noreply.github.com>
2020-12-19 02:47:14 +0900

    todays ahchoo 追加

--- front/React/mapviewer/src/Main.js
+++ front/React/mapviewer/src/Main.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class Main extends React.Component {
+
+  componentWillMount() {
+    return fetch('http://192.168.20.155:8080/get')
+      .then((response) => response.json())
+      .then((responseJson) => (console.log("get"))
+      )
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <div className='main'>
--- front/React/mapviewer/src/Sidebar.js
+++ front/React/mapviewer/src/Sidebar.js
@@ -1,11 +1,37 @@
 import React from 'react';
 
+class AhchooCounter extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      dayOf:0,
+      weekOf:0,
+      monthOf:0
+    }
+  }
+
+  render(){
+    return(
+       <div className="menu">
+         <h1 className="menuTitle">今日のAHCHOO!</h1>
+         <h2 className="menuValue">{this.state.dayOf} 回</h2>
+         <h1 className="menuTitle">今週のAHCHOO!</h1>
+         <h2 className="menuValue">{this.state.weekOf} 回</h2>
+         <h1 className="menuTitle">今月のAHCHOO!</h1>
+         <h2 className="menuValue">{this.state.monthOf} 回</h2>
+      </div>
+    );
+  }
+}
+
+
 class Header extends React.Component {
   render() {
     return (
       <div className='sidebar'>
           <p className="title">AHCHOO!</p>
           <p className="sub">くしゃみ/咳のマップビューワー</p>
+          <AhchooCounter></AhchooCounter>
       </div>
     );
   }
